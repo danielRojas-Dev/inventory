dddef929ccdb518f50c96faf19c3b9558d3c07eb
danielRojasDev <danielrojas131415@gmail.com>
2024-12-10 00:05:17 -0300

    Se esat adaptando el sistema para electro dr

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,60 @@
 @extends('auth.body.main')
 
 @section('container')
-<div class="row align-items-center justify-content-center height-self-center">
-    <div class="col-lg-8">
-        <div class="card auth-card">
-            <div class="card-body p-0">
-                <div class="d-flex align-items-center auth-content">
-                    <div class="col-lg-7 align-self-center">
-                        <div class="p-3">
-
-                            <h2 class="mb-2">Log In</h2>
-                            <p>Login to stay connected.</p>
-
-                            <form action="{{ route('login') }}" method="POST">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <div class="floating-label form-group">
-                                            <input class="floating-input form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror" type="text" name="input_type" placeholder=" " value="{{ old('input_type') }}" autocomplete="off" required autofocus>
-                                            <label>Email/Username</label>
+    <div class="row align-items-center justify-content-center min-vh-100">
+        <div class="col-lg-4">
+            <div class="card auth-card">
+                <div class="card-body p-0">
+                    <div class="d-flex align-items-center auth-content">
+                        <div class="col-lg-12 align-self-center">
+                            <div class=" text-center">
+                                <img src="{{ asset('assets/images/login/MIA.png') }}" class="img-fluid"
+                                    style="max-width: 60%; height: auto;" alt="">
+                            </div>
+                            <div class="">
+                                <h2 class="mb-2 text-center">Iniciar sesión</h2>
+                                <p class="text-center">Inicia sesión para mantenerte conectado.</p>
+                                <form action="{{ route('login') }}" method="POST">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="floating-label form-group">
+                                                <input
+                                                    class="floating-input form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror"
+                                                    type="text" name="input_type" placeholder=" "
+                                                    value="{{ old('input_type') }}" autocomplete="off" required autofocus>
+                                                <label>Email/Nombre de usuario</label>
+                                            </div>
+                                            @error('username')
+                                                <div class="mb-4" style="margin-top: -20px">
+                                                    <div class="text-danger small">Nombre de usuario o contraseña incorrectos.
+                                                    </div>
+                                                </div>
+                                            @enderror
+                                            @error('email')
+                                                <div class="mb-4" style="margin-top: -20px">
+                                                    <div class="text-danger small">Correo electrónico o contraseña incorrectos.
+                                                    </div>
+                                                </div>
+                                            @enderror
                                         </div>
-                                        @error('username')
-                                        <div class="mb-4" style="margin-top: -20px">
-                                            <div class="text-danger small">Incorrect username or password.</div>
-                                        </div>
-                                        @enderror
-                                        @error('email')
-                                        <div class="mb-4" style="margin-top: -20px">
-                                            <div class="text-danger small">Incorrect username or password.</div>
-                                        </div>
-                                        @enderror
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <div class="floating-label form-group">
-                                            <input class="floating-input form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror" type="password" name="password" placeholder=" " required>
-                                            <label>Password</label>
+                                        <div class="col-lg-12">
+                                            <div class="floating-label form-group">
+                                                <input
+                                                    class="floating-input form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror"
+                                                    type="password" name="password" placeholder=" " required>
+                                                <label>Contraseña</label>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-6">
-                                        <p>
-                                            Not a Member yet? <a href="{{ route('register') }}" class="text-primary">Register</a>
-                                        </p>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <a href="#" class="text-primary float-right">Forgot Password?</a>
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Login</button>
-                            </form>
+                                    <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-lg-5 content-right">
-                        <img src="{{ asset('assets/images/login/01.png') }}" class="img-fluid image-right" alt="">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
