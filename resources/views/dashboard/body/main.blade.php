6b0741891fc81b4b69b6dcf394e07e67fa3e8438
danielRojasDev <danielrojas131415@gmail.com>
2024-12-11 19:58:38 -0300

    adap

--- resources/views/dashboard/body/main.blade.php
+++ resources/views/dashboard/body/main.blade.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" href="{{ asset('assets/images/login/logodr.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/backend.css?v=1.0.0') }}">
-
+    <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/fonts/remixicon.css') }}">
 
