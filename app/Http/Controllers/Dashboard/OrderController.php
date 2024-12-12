1f0977afc9d6ae569fb9a216d8dd296853e61e4e
danielRojasDev <danielrojas131415@gmail.com>
2024-12-11 21:16:25 -0300

    ventas

--- app/Http/Controllers/Dashboard/OrderController.php
+++ app/Http/Controllers/Dashboard/OrderController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\Dashboard;
 
 use App\Models\Order;
@@ -27,7 +26,7 @@ class OrderController extends Controller
             abort(400, 'El parámetro por página debe ser un número entero entre 1 y 100.');
         }
 
-        $orders = Order::where('order_status', 'pending')->sortable()->paginate($row);
+        $orders = Order::where('order_status', 'Pendiente')->where('due', '0')->sortable()->paginate($row);
 
         return view('orders.pending-orders', [
             'orders' => $orders
@@ -42,7 +41,7 @@ class OrderController extends Controller
             abort(400, 'El parámetro por página debe ser un número entero entre 1 y 100.');
         }
 
-        $orders = Order::where('order_status', 'complete')->sortable()->paginate($row);
+        $orders = Order::where('order_status', 'complete')->orderBy('order_date', 'desc')->sortable()->paginate($row);
 
         return view('orders.complete-orders', [
             'orders' => $orders
@@ -71,52 +70,59 @@ class OrderController extends Controller
      */
     public function storeOrder(Request $request)
     {
-        $rules = [
-            'customer_id' => 'required|numeric',
-            'payment_status' => 'required|string',
-            'pay' => 'numeric|nullable',
-            'due' => 'numeric|nullable',
-        ];
+        try {
+            $rules = [
+                // 'customer_id' => 'required|numeric',
+                'payment_status' => 'required|string',
+                // 'pay' => 'numeric|nullable',
+                'due' => 'numeric|nullable',
+            ];
 
-        $invoice_no = IdGenerator::generate([
-            'table' => 'orders',
-            'field' => 'invoice_no',
-            'length' => 10,
-            'prefix' => 'INV-'
-        ]);
+            $invoice_no = IdGenerator::generate([
+                'table' => 'orders',
+                'field' => 'invoice_no',
+                'length' => 10,
+                'prefix' => 'INV-'
+            ]);
+            $validatedData = $request->validate($rules);
+            $validatedData['order_date'] = Carbon::now()->format('Y-m-d H:i:s');
+            $validatedData['order_status'] = 'Pendiente';
+            $validatedData['total_products'] = Cart::count();
+            $validatedData['sub_total'] = Cart::subtotal();
+            $validatedData['vat'] = Cart::tax();
+            $validatedData['invoice_no'] = $invoice_no;
+            $validatedData['total'] = Cart::total();
+            $validatedData['due'] = Cart::total();
+            $validatedData['pay'] = 0;
+            // $validatedData['due'] = Cart::total() - $validatedData['pay'];
+            $validatedData['created_at'] = Carbon::now();
+            $validatedData['created_at'] = Carbon::now();
+            $validatedData['user_id'] = auth()->id(); // ID del usuario logueado
 
-        $validatedData = $request->validate($rules);
-        $validatedData['order_date'] = Carbon::now()->format('Y-m-d');
-        $validatedData['order_status'] = 'pending';
-        $validatedData['total_products'] = Cart::count();
-        $validatedData['sub_total'] = Cart::subtotal();
-        $validatedData['vat'] = Cart::tax();
-        $validatedData['invoice_no'] = $invoice_no;
-        $validatedData['total'] = Cart::total();
-        $validatedData['due'] = Cart::total() - $validatedData['pay'];
-        $validatedData['created_at'] = Carbon::now();
+            $order_id = Order::insertGetId($validatedData);
 
-        $order_id = Order::insertGetId($validatedData);
+            // Create Order Details
+            $contents = Cart::content();
+            $oDetails = array();
 
-        // Create Order Details
-        $contents = Cart::content();
-        $oDetails = array();
+            foreach ($contents as $content) {
+                $oDetails['order_id'] = $order_id;
+                $oDetails['product_id'] = $content->id;
+                $oDetails['quantity'] = $content->qty;
+                $oDetails['unitcost'] = $content->price;
+                $oDetails['total'] = $content->total;
+                $oDetails['created_at'] = Carbon::now();
 
-        foreach ($contents as $content) {
-            $oDetails['order_id'] = $order_id;
-            $oDetails['product_id'] = $content->id;
-            $oDetails['quantity'] = $content->qty;
-            $oDetails['unitcost'] = $content->price;
-            $oDetails['total'] = $content->total;
-            $oDetails['created_at'] = Carbon::now();
+                OrderDetails::insert($oDetails);
+            }
 
-            OrderDetails::insert($oDetails);
+            // Delete Cart Sopping History
+            Cart::destroy();
+
+            return Redirect::route('dashboard')->with('success', '¡Se ha creado la Venta!');
+        } catch (\Throwable $th) {
+            dd($th);
         }
-
-        // Delete Cart Sopping History
-        Cart::destroy();
-
-        return Redirect::route('dashboard')->with('success', 'Order has been created!');
     }
 
     /**
@@ -126,9 +132,9 @@ class OrderController extends Controller
     {
         $order = Order::where('id', $order_id)->first();
         $orderDetails = OrderDetails::with('product')
-                        ->where('order_id', $order_id)
-                        ->orderBy('id', 'DESC')
-                        ->get();
+            ->where('order_id', $order_id)
+            ->orderBy('id', 'DESC')
+            ->get();
 
         return view('orders.details-order', [
             'order' => $order,
@@ -148,21 +154,21 @@ class OrderController extends Controller
 
         foreach ($products as $product) {
             Product::where('id', $product->product_id)
-                    ->update(['product_store' => DB::raw('product_store-'.$product->quantity)]);
+                ->update(['product_store' => DB::raw('product_store-' . $product->quantity)]);
         }
 
         Order::findOrFail($order_id)->update(['order_status' => 'complete']);
 
-        return Redirect::route('order.pendingOrders')->with('success', 'Order has been completed!');
+        return Redirect::route('order.pendingOrders')->with('success', '¡La orden ha sido completada!');
     }
 
     public function invoiceDownload(Int $order_id)
     {
         $order = Order::where('id', $order_id)->first();
         $orderDetails = OrderDetails::with('product')
-                        ->where('order_id', $order_id)
-                        ->orderBy('id', 'DESC')
-                        ->get();
+            ->where('order_id', $order_id)
+            ->orderBy('id', 'DESC')
+            ->get();
 
         // show data (only for debugging)
         return view('orders.invoice-order', [
@@ -197,6 +203,7 @@ class OrderController extends Controller
 
     public function updateDue(Request $request)
     {
+        // Validaciones
         $rules = [
             'order_id' => 'required|numeric',
             'due' => 'required|numeric',
@@ -204,18 +211,30 @@ class OrderController extends Controller
 
         $validatedData = $request->validate($rules);
 
+        // Buscar la orden
         $order = Order::findOrFail($request->order_id);
         $mainPay = $order->pay;
         $mainDue = $order->due;
 
+        // Calcular el nuevo valor de due y pay
         $paid_due = $mainDue - $validatedData['due'];
         $paid_pay = $mainPay + $validatedData['due'];
 
-        Order::findOrFail($request->order_id)->update([
+        // Verificar si el monto pendiente es 0, para actualizar el estado de la orden
+        $order_status = $paid_due <= 0 ? 'complete' : $order->order_status;
+
+        // Actualizar la orden
+        $order->update([
             'due' => $paid_due,
             'pay' => $paid_pay,
+            'order_status' => $order_status, // Cambiar el estado si es necesario
         ]);
 
-        return Redirect::route('order.pendingDue')->with('success', 'Due Amount Updated Successfully!');
+        if ($order_status == 'complete') {
+            return Redirect::route('order.completeOrders')->with('success', '¡Pago Completado con éxito!');
+        }
+
+        // Redirigir con un mensaje de éxito
+        return Redirect::route('order.pendingDue')->with('success', '¡Importe adeudado actualizado correctamente!');
     }
 }
