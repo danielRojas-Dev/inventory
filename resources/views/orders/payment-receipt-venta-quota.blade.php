760586b7437f8d902d6f40b49dec62b3f70255e5
danielRojasDev <danielrojas131415@gmail.com>
2025-02-24 22:06:09 -0300

    fix

--- resources/views/orders/payment-receipt-venta-quota.blade.php
+++ resources/views/orders/payment-receipt-venta-quota.blade.php
@@ -199,125 +199,129 @@
 
     </div>
     -----------------------------------------------------------------------------------------------------------------------------------------------
-    {{-- <div class="nota-pedido">
-        <div class="fecha">
-            <div class="content-fecha" style="padding-top: 15px;">
-                <small>N°: <b>{{ $order->invoice_no }}</b></small>
-                <br>
-                <small>Fecha: <b>{{ date('d-m-Y', strtotime($order->order_date)) }}</b></small>
-                <br>
-                <small>Tel: <b>3704-590488</b></small>
+
+    @if (!$isEntregaInicial)
+        <div class="nota-pedido">
+            <div class="fecha">
+                <div class="content-fecha" style="padding-top: 15px;">
+                    <small>N°: <b>{{ $order->invoice_no }}</b></small>
+                    <br>
+                    <small>Fecha: <b>{{ date('d-m-Y', strtotime($order->order_date)) }}</b></small>
+                    <br>
+                    <small>Tel: <b>3704-590488</b></small>
+                </div>
             </div>
-        </div>
-        <div style="display: inline-block; width: 30%; text-align: left; margin-top: 30px;">
-            <div class="logo">
-                {!! $htmlLogo !!}
+            <div style="display: inline-block; width: 30%; text-align: left; margin-top: 30px;">
+                <div class="logo">
+                    {!! $htmlLogo !!}
+                </div>
             </div>
-        </div>
 
-        <div style="display: inline-block; width: 35%; ">
-            <div class="title">
-                {!! $htmlTitle !!}
+            <div style="display: inline-block; width: 35%; ">
+                <div class="title">
+                    {!! $htmlTitle !!}
+                </div>
             </div>
-        </div>
 
-        <div>COMPROBANTE DE VENTA</div>
+            <div>COMPROBANTE DE VENTA</div>
 
-        <div class="header"></div>
-        <table style="width: 100%;" class="info">
-            <tr>
-                <!-- Primera columna -->
-                <td style="text-align: left; vertical-align: top; width: 33%;">
-                    <strong>Cliente:</strong> {{ $cliente->name }}<br>
-                    <strong>Documento N°:</strong> {{ $cliente->dni }}<br>
-                    <strong>Teléfono:</strong> {{ $cliente->phone }}
-
-                </td>
-
-                <!-- Segunda columna -->
-                <td style="text-align: left; vertical-align: top; width: 33%;">
-                    <strong>Domicilio:</strong> {{ $cliente->address }} <br>
-                    <strong>Localidad:</strong> {{ $cliente->city }}
-                </td>
-            </tr>
-        </table>
-
-        @if ($isEntregaInicial)
-            <table style="width: 100%; border-collapse: collapse; text-align: center; margin-bottom: 5px;">
+            <div class="header"></div>
+            <table style="width: 100%;" class="info">
                 <tr>
-                    <td><strong>Entrega Inicial: </strong>
-                        <strong>${{ number_format($primerCuota, 0, ',', '.') }}</strong>
+                    <!-- Primera columna -->
+                    <td style="text-align: left; vertical-align: top; width: 33%;">
+                        <strong>Cliente:</strong> {{ $cliente->name }}<br>
+                        <strong>Documento N°:</strong> {{ $cliente->dni }}<br>
+                        <strong>Teléfono:</strong> {{ $cliente->phone }}
+
+                    </td>
+
+                    <!-- Segunda columna -->
+                    <td style="text-align: left; vertical-align: top; width: 33%;">
+                        <strong>Domicilio:</strong> {{ $cliente->address }} <br>
+                        <strong>Localidad:</strong> {{ $cliente->city }}
                     </td>
                 </tr>
             </table>
-        @endif
 
-
-        <div class="section">
-            <table style="width: 100%; border-collapse: collapse; text-align: center;">
-                <tr>
-                    <th style="border: 1px solid black; padding: 1px; text-align: center;">Cantidad</th>
-                    <th style="border: 1px solid black; padding: 1px; text-align: center;">Descripción</th>
-                    <th style="border: 1px solid black; padding: 1px; text-align: center;">Marca</th>
-                    <th style="border: 1px solid black; padding: 1px; text-align: center;">Plan de Cuotas</th>
-                    <th style="border: 1px solid black; padding: 1px; text-align: center;">Monto de Cuotas</th>
-                </tr>
-                @foreach ($details as $detail)
+            @if ($isEntregaInicial)
+                <table style="width: 100%; border-collapse: collapse; text-align: center; margin-bottom: 5px;">
                     <tr>
-                        <td style="border: 1px solid black; padding: 1px; text-align: center;">{{ $detail->quantity }}
-                        </td>
-                        <td style="border: 1px solid black; padding: 1px; text-align: center;">
-                            {{ $detail->product->product_name }}</td>
-                        <td style="border: 1px solid black; padding: 1px; text-align: center;">
-                            {{ $detail->product->brand->name }}</td>
-                        <td style="border: 1px solid black; padding: 1px; text-align: center;">
-                            @if ($isEntregaInicial)
-                                {{ $order->quotas - 1 }}
-                            @else
-                                {{ $order->quotas }}
-                            @endif
-                        </td>
-                        <td style="border: 1px solid black; padding: 1px; text-align: center;">$
-                            {{ number_format($valorCuota, 0, ',', '.') }}
+                        <td><strong>Entrega Inicial: </strong>
+                            <strong>${{ number_format($primerCuota, 0, ',', '.') }}</strong>
                         </td>
                     </tr>
-                @endforeach
-            </table>
+                </table>
+            @endif
+
+
+            <div class="section">
+                <table style="width: 100%; border-collapse: collapse; text-align: center;">
+                    <tr>
+                        <th style="border: 1px solid black; padding: 1px; text-align: center;">Cantidad</th>
+                        <th style="border: 1px solid black; padding: 1px; text-align: center;">Descripción</th>
+                        <th style="border: 1px solid black; padding: 1px; text-align: center;">Marca</th>
+                        <th style="border: 1px solid black; padding: 1px; text-align: center;">Plan de Cuotas</th>
+                        <th style="border: 1px solid black; padding: 1px; text-align: center;">Monto de Cuotas</th>
+                    </tr>
+                    @foreach ($details as $detail)
+                        <tr>
+                            <td style="border: 1px solid black; padding: 1px; text-align: center;">
+                                {{ $detail->quantity }}
+                            </td>
+                            <td style="border: 1px solid black; padding: 1px; text-align: center;">
+                                {{ $detail->product->product_name }}</td>
+                            <td style="border: 1px solid black; padding: 1px; text-align: center;">
+                                {{ $detail->product->brand->name }}</td>
+                            <td style="border: 1px solid black; padding: 1px; text-align: center;">
+                                @if ($isEntregaInicial)
+                                    {{ $order->quotas - 1 }}
+                                @else
+                                    {{ $order->quotas }}
+                                @endif
+                            </td>
+                            <td style="border: 1px solid black; padding: 1px; text-align: center;">$
+                                {{ number_format($valorCuota, 0, ',', '.') }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+
+
+
+
+            <?php
+            $fecha = $estimatedPaymentDate;
+            [$anio, $mes, $dia] = explode('-', $fecha);
+            ?>
+
+            <?php
+            $fecha = $estimatedPaymentDate;
+            [$anio, $mes, $dia] = explode('-', $fecha);
+            ?>
+
+            <div class="important">
+                <p><strong>Nota Importante:</strong> Por falta de cualquier mensualidad nos reservamos el derecho de
+                    cobrar
+                    el total del saldo de la venta.
+                    <br> En caso de vencimiento de las cuotas, se aplicará un interés del
+                    <b>1.5%</b> diario sobre el valor de la cuota hasta su cancelación. <br>Sírvase avisar el cambio de
+                    domicilio.
+                </p>
+
+                <p><strong>Compromiso de pago:</strong> Conste por la presente que me comprometo a abonar la Mercadería
+                    detallada en <b>{{ $order->quotas }}</b> cuotas fijas de $
+                    <b>{{ number_format($valorCuota, 0, ',', '.') }}</b> a
+                    partir del día <b>{{ $dia }}</b> del <b>{{ $mes }}</b>
+                    del año <b>{{ $anio }}</b>.
+                </p>
+                <p class="firma"><strong>Firma:</strong> ____________________</p>
+            </div>
+
+
         </div>
-
-
-
-
-        <?php
-        $fecha = $estimatedPaymentDate;
-        [$anio, $mes, $dia] = explode('-', $fecha);
-        ?>
-
-        <?php
-        $fecha = $estimatedPaymentDate;
-        [$anio, $mes, $dia] = explode('-', $fecha);
-        ?>
-
-        <div class="important">
-            <p><strong>Nota Importante:</strong> Por falta de cualquier mensualidad nos reservamos el derecho de
-                cobrar
-                el total del saldo de la venta.
-                <br> En caso de vencimiento de las cuotas, se aplicará un interés del
-                <b>1.5%</b> diario sobre el valor de la cuota hasta su cancelación. <br>Sírvase avisar el cambio de
-                domicilio.
-            </p>
-
-            <p><strong>Compromiso de pago:</strong> Conste por la presente que me comprometo a abonar la Mercadería
-                detallada en <b>{{ $order->quotas }}</b> cuotas fijas de $
-                <b>{{ number_format($valorCuota, 0, ',', '.') }}</b> a
-                partir del día <b>{{ $dia }}</b> del <b>{{ $mes }}</b>
-                del año <b>{{ $anio }}</b>.
-            </p>
-            <p class="firma"><strong>Firma:</strong> ____________________</p>
-        </div>
-
-
-    </div> --}}
+    @endif
 </body>
 
 </html>
