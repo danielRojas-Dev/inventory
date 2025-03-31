2241c1edd4265a24e3b00d97d70e140799862709
danielRojasDev <danielrojas131415@gmail.com>
2025-03-31 11:59:22 -0300

    fix fecha prestamo

--- resources/views/loans/payment-receipt-loan.blade.php
+++ resources/views/loans/payment-receipt-loan.blade.php
@@ -79,7 +79,7 @@
             <div class="content-fecha" style="padding-top: 15px;">
                 <small>N°: <b>{{ $loan->invoice_no }}</b></small>
                 <br>
-                <small>Fecha: <b>{{ date('d-m-Y', strtotime($loan->order_date)) }}</b></small>
+                <small>Fecha: <b>{{ $loan_date }}</b></small>
                 <br>
                 <small>Tel: <b>3704-590488</b></small>
             </div>
