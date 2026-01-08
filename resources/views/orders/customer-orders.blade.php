067fd1a0ea552677b8c9551ca1de0162a9b074d8
daniel.rojas <daniel.rojas@smartway.com.ar>
2026-01-07 22:48:48 -0300

    fix cambio de texto de botón "Pagar Cuotas" a "Ver detalle" en órdenes de clientes

--- resources/views/orders/customer-orders.blade.php
+++ resources/views/orders/customer-orders.blade.php
@@ -122,7 +122,7 @@
                                 <div class="d-flex flex-wrap align-items-center gap-2">
                                     <h6 class="mb-0">Tiene cuotas Asociadas:</h6>
                                     <a href="{{ Route('order.quotas', $order->id) }}" class="btn btn-success btn-sm">
-                                        Pagar Cuotas
+                                        Ver detalle
                                     </a>
                                 </div>
                             @endif
